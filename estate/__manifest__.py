5efe27a5d9fc627fbc31ee9dd58b6b9c5bce3722
Erik Korbel <erikkorbel@gmail.com>
2025-04-17 15:24:11 +0200

    chapter 2 - done

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'estate',
+    'application': True,
+    'website': 'https://www.odoo.com/page/estate',
+    'depends': [
+        'base'
+    ],
+}
