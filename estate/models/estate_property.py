997acfa0b2f52a98e52ba57c5fc01a7681009e0f
Erik Korbel <erikkorbel@gmail.com>
2025-04-17 19:40:56 +0200

    chapter 6 - done

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,18 +7,18 @@ class EstateProperty(models.Model):
     _description = "Estate Property"
     _order = "name"
 
-    name = fields.Char("Property Name", required=True)
+    name = fields.Char("Name", required=True)
     description = fields.Text("Property Description")
     postcode = fields.Char("Postcode")
-    date_available = fields.Date("Date Available", copy=False, default=datetime.today() + timedelta(days=90))
+    date_available = fields.Date("Available From", copy=False, default=datetime.today() + timedelta(days=90))
     expected_price = fields.Float("Expected Price", required=True)
     selling_price = fields.Float("Selling Price", readonly=True, copy=False)
     bedrooms = fields.Integer("Bedrooms", default=2)
-    living_area = fields.Integer("Living Area")
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
-    garden_area = fields.Integer("Garden Area")
+    garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(
         string="Garden Orientation",
         selection=[
